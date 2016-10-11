f4acba71005023483735d364e9830dce49b744a6
R. Serge from farpost <mail@r-serge.info>
2016-10-11 17:30:50 +1000

    Add domain create

--- python/mailMGM/mailMGM.py
+++ python/mailMGM/mailMGM.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import re
+import os, sys, re
 # Argument parse lib
 import argparse
+import random
 
 #MAILBOX_DIR = "/var/vmail"
-MAILBOX_DIR = "./testmaildir"
+MAILBOX_DIR = "/home/candidate/repos/scripts/python/mailMGM/testmaildir"
 #MAILCONFIG_DIR = "/etc/dovecot/vmail"
-MAILCONFIG_DIR = "./testdir"
+MAILCONFIG_DIR = "/home/candidate/repos/scripts/python/mailMGM/testdir"
 
 result = {}
 
-# create parsere object
+# create parser object
 parser = argparse.ArgumentParser(description="Add a mailbox or a domain into exim+dovecot")
 
 # Adding arguments 
@@ -21,7 +20,7 @@ parser.add_argument("mailaddr",
 					help="email in user@domain format or domain")
 
 parser.add_argument("-c",
-					"--create-domains",
+					"--create",
 					action="store_true",
 					help="Create non-exist domains")
 
@@ -29,6 +28,7 @@ args = parser.parse_args()
 
 # Divide mailadd arg into mail and domain parts
 # TODO: make a check for valid input for mail and domains
+# TODO: make all lowercase
 mail, domain = re.split("@",args.mailaddr)
 
 # print "Mail is "+mail, "Domain is "+domain
@@ -44,25 +44,48 @@ except Exception as e:
 
 # check existent domains 
 # TODO - add "create" flag support
-if domain not in domains:
+if (domain not in domains) and not args.create:
 	result[args.mailaddr] = {
 		"result" : 0,
 		"reason" : "domain is not exists"
 	}
 	print result
 	exit(1)
+elif(args.create):
+	try:
+		# Create directories and passwd\alias files
+		os.mkdir("MAILCONFIG_DIR"+"/"+domain)
+		os.mkdir("MAILBOX_DIR"+"/"+domain)
+
+		open("MAILCONFIG_DIR"+"/"+domain+"/passwd",'a').close()
+		open("MAILCONFIG_DIR"+"/"+domain+"/aliases",'a').close()
+	except Exception as e:
+		result[args.mailaddr] = {
+			"result" : 0,
+			"reason" : "domain not created",
+			"error"  : e
+		}
+		print result
 
 # Get already created usernames for domain
 try:
 	with open (MAILCONFIG_DIR+"/"+domain+"/passwd","r") as passwdfile: 
+		# TODO: use match or search - 
 		usernames = (re.split(r":",lines,maxsplit=1) for lines in passwdfile)
-		# if mail in usernames:
-		# 	result[args.mailaddr] = {
-		# 		"result" : 0,
-		# 		"reason" : "user "+mail+" is exists"
-		# 	}
-		for name in usernames:
-			print name[0]
+
 except Exception as e:
 	print e
 	exit(1)
+
+# Check if user is exist
+for name in usernames:
+	if mail == name[0]:
+		result[args.mailaddr] = {
+			"result" : 0,
+			"reason" : "user "+mail+" is exists"
+		}
+		print result
+		exit(1)
+
+
+# create user
