fda1529285aa48a6d05597d6a6afddef8e645a8f
Serge R. extreme <mail@r-serge.info>
2016-10-12 17:52:44 +1000

    add some functions for mail

--- python/mailMGM/mailMGM.py
+++ python/mailMGM/mailMGM.py
@@ -4,15 +4,23 @@ import os, sys
 import argparse
 import random
 import subprocess
+import smtplib   
+from email.mime.text import MIMEText    
 
 MAILBOX_DIR = "./mailDir"
 MAILCONFIG_DIR = "./mailConf"
 PASS_LEN=12
+CRYPT_SCHEME="SHA512-CRYPT"
+
+POSTMASTER_ADDRESS="postmaster@localhost"
+SMTP_SERVER="localhost"
+SUBJECT="Welcome"
+TEXT="Welcome" + user +" to " + domain + "!!!"
 
 result = {}
 
-def randomPass (len=PASS_LEN):
-	""" Creates random string """
+def randomPass(len=PASS_LEN):
+	""" Creates and return random string """
 	digits = "0123456789"
 	liters = "abcdefghijklmnopqrstuvwxyz+--()"
 	upLiters = liters.upper()
@@ -22,13 +30,27 @@ def randomPass (len=PASS_LEN):
 	password = "".join(random.choice(literList) for x in xrange(len))
 	return password
 
-def cryptUser (user, passwd):
-	""" Call dovecot adm utility for create crypted string
+def cryptUser(user, passwd):
+	""" Call dovecot adm utility for create and return crypted string
 	TODO: try generate string by python """
 	PIPE = subprocess.PIPE
-	proc = subprocess.Popen(['doveadm', 'pw', '-s', 'SHA512-CRYPT', '-p', passwd],  stdout = PIPE)
+	proc = subprocess.Popen(['doveadm', 'pw', '-s', CRYPT_SCHEME, '-p', passwd],  stdout = PIPE)
 	string = user + ":" + proc.communicate()[0]
 	return string
+
+def sendMail(who):
+	""" Send mail to new user for creating emails dir """
+	msg = MIMEText(TEXT)
+	msg["Subject"] = SUBJECT
+	msg["From"] = POSTMASTER_ADDRESS
+	msg["To"] = who
+	try:
+		server = smtplib(SMTP_SERVER)
+		sendmail(POSTMASTER_ADDRESS, [who], msg.as_string())
+		server.quit()
+		return 1
+	except Exception as e:
+		return 0, e
 
 # create parser object
 parser = argparse.ArgumentParser(description="Add a mailbox or a domain into exim+dovecot")
@@ -46,7 +68,7 @@ args = parser.parse_args()
 
 # Divide mailadd arg into mail and domain parts
 # TODO: make a check for valid input for mail and domains
-mail, domain = args.mailaddr.lower().split("@")
+user, domain = args.mailaddr.lower().split("@")
 
 # Get already created domains
 # Fail if MAILCONFIG_DIR is not set 
@@ -81,6 +103,7 @@ elif(args.create):
 		}
 		print result
         exit(1)
+        
 # Get already created usernames for domain
 # And create if user not exist
 try:
@@ -88,7 +111,7 @@ try:
 		# TODO: use match or search -  
 		lines = passwdfile.readlines()
 		users = (line.split(":")[0] for line in lines)
-		if mail in users:
+		if user in users:
 			# TODO: add result output
 			print "user exists"
 			exit(1)
