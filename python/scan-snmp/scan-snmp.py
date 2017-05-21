4da6386ad7f75890b2eaf22e6a6c2d37e1da9e35
root <root@vt-test-srv.vladtelecom.net>
2017-05-21 16:03:16 +1000

    Draft - rewrite to python3.6

--- python/scan-snmp/scan-snmp.py
+++ python/scan-snmp/scan-snmp.py
@@ -1,99 +1,82 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 """
  Scan network via fping and SNMP
- ver. 1.0
+ ver. 1.1
  Write html file "Report.html"
 
- Needs Python 2.7, html, netaddr, netsnmp libs
+ Needs Python 3.6 jinja2, netaddr, pysnmp, multiprocess
 
 """
 
-"""
-Import section
-
-"""
-# SNMP
-import netsnmp
-
-# Forf ping
+from jinja2 import Environment, FileSystemLoader
+from pysnmp.hlapi import *
+from datetime import datetime
 import os
-
-# For easy create table
-from html import HTML
-
-# For ip mgmt
-from netaddr import *
-
-# For test elasped time
+import netaddr as na
 import time
+import re
+import multiprocessing as mp
 
 # MGMT network addresses
-mgmtNetwork = list(iter_iprange("192.168.0.1","192.168.0.254"))
-
-# Report filename
-reportFile = "report.html"
+mgmtNetwork = list(na.iter_iprange("192.168.248.1","192.168.255.254"))
 
 # Community string
 community = "public"
 
-# SNMP Vars
-#snmpVarSerial = netsnmp.Varbind('SNMPv2-SMI::enterprises.171.12.1.1.12.0')
-snmpVarName = netsnmp.Varbind('SNMPv2-MIB::sysName.0')
-snmpVarLocation = netsnmp.Varbind('SNMPv2-MIB::sysLocation.0')
-snmpVarDescr = netsnmp.Varbind('SNMPv2-MIB::sysDescr.0')
+switches = []
 
-# Base Counter
-counter=0
+# Init Template
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+env = Environment(loader = FileSystemLoader(curr_dir))
+template = env.get_template('template.j2')
 
-# Models dict
-models = {}
+# Current time
+start = time.time()
+keys = ['descr','location','name','ip']
 
-# init HTML operations
-h = HTML()
+for ip in mgmtNetwork:
+    if (os.system("fping " +str(ip) +" -r0 -t10 1>/dev/null") == 0):
 
-# Open report file to write
-with open(reportFile,'w') as report:
-    report.writeli nes("<!DOCTYPE html>")
-    report.writelines("<html>")
-    report.writelines("\t<head><title>REPORT</title></head>")
-    report.writelines("\t<body>")
-    table = h.table(border='1')
-    r = table.tr
-    r.td("IP")
-    r.td("Name")
-    r.td("Description")
-    r.td("Locarion")
-    r.td("Serial")
-    start = time.time()
-    for ip in mgmtNetwork:
-        
-        if (os.system("fping " +str(ip) +" -r0 -t100 1>/dev/null") == 0):
-            # for debug
-            print (str(ip))
+        # for debug
+        print(str(ip))
 
-            r = table.tr
-            name = netsnmp.snmpget(snmpVarName,Version=2,Timeout=5000,Retries=0,DestHost=str(ip),Community=community)
-            descr = netsnmp.snmpget(snmpVarDescr,Version=2,Timeout=5000,Retries=0,DestHost=str(ip),Community=community)
-            location = netsnmp.snmpget(snmpVarLocation,Version=2,Timeout=5000,Retries=0,DestHost=str(ip),Community=community)
-            #  serial = netsnmp.snmpget(snmpVarSerial,Version=2,Timeout=5000,Retries=0,DestHost=str(ip),Community=community)
-            r.td(str(ip))
-            r.td(str(name))
-            r.td(str(descr))
-            r.td(str(location))
-            # r.td(str(serial))
-            counter +=  1
-            if (descr != (None,)):
-                if models.get(str(descr[0])) == None:
-                    models[str(descr[0])] = 1
-                else:
-                     models[str(descr[0])] +=1
-    r = table.tr
-    r.td("<b>Summ:</b>")
-    r.td("<b>"+str(counter)+"</b")
-    report.writelines(table)
-    report.writelines("\t</body>")
-    report.writelines("</html")
-    print("\nElasped time: {:.4f}".format(time.time() - start))
-    print (models)
+        # SNMP Vars
+        errorIndication, errorStatus, errorIndex, varBinds = next(
+                getCmd(SnmpEngine(),
+                    CommunityData(community),
+                    UdpTransportTarget((str(ip), 161),timeout=1,retries=0),
+                    ContextData(),
+                    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)),
+                    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysLocation', 0)),
+                    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysName', 0)))
+                   # ObjectType(ObjectIdentity('SNMPv2-SMI', 'enterprises.171.12.1.1.12', 0)))
+        )
+
+        if errorIndication:
+            print(errorIndication)
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                 errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        else:
+            values = [re.sub(r'\s+',' ',str(val[1])) for val in varBinds]
+            values.append(str(ip))
+            switch = {key:val for key,val in zip(keys,values)}
+            switches.append(switch)
+
+# Print models only
+all_models = [switch['descr'] for switch in switches]
+unique_models = list(set(all_models))
+models_dict = []
+
+for model in unique_models:
+    models_dict.append({'name':model, 'count':all_models.count(model)})
+
+print(models_dict)
+with open("report.html",'w') as report:
+    time_now = str(datetime.now())
+    report.write(template.render(sw = switches,
+                                 models=models_dict,
+                                 time=time_now,
+                                 count=len(all_models)))
+    print("File writed")
+
+print("Elasped time: {:.4f}".format(time.time() - start))
